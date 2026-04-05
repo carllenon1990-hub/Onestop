e00122755883d77e36a19f2bd6131aa5550f2a7b
carllenon1990-hub <carllenon1990@gmail.com>
2026-04-05 06:40:59 -0400

    Update _app.js with new analytics functionality.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <SpeedInsights />
       <Component {...pageProps} />
       <Analytics />
     </>
